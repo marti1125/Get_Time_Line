b1df59f03fca89657cb80ca19df1db9c9a78b2bf
Willy Aguirre <marti1125@gmail.com>
2013-12-07 23:47:15 -0500

    add twit to package depencie and another services

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Get Time Line
 
 Twitter get user time line
 
+    http://localhost:8080/
+
     /lineaunope
+    /USGSted
+    /USGSBigQuakes
--- service.js
+++ service.js
@@ -10,9 +10,34 @@ var twitter = new Twit({
   access_token_secret: 's2R7YOJWMWYkQQk57KbNUkqwoA8VoH8eBfvi4VeNY'
 });
 
+route.get('/', function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write('<!doctype html>\n<html lang="en">\n' +
+ '<head>\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' +
+ '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
+ '</head>\n<body>\n<h1>Get Time Line</h1>\n' +
+ '<div id="content"><p>List of users:</p><ul><li>/lineaunope</li><li>/USGSted</li><li>/USGSBigQuakes</li></ul></div>' +
+ '\n</body>\n</html>');  
+  response.end();  
+});
+
 route.get('/lineaunope', function(request, response) {
 	response.writeHead(200, {"Content-Type": "application/json"});
 	twitter.get('statuses/user_timeline', { screen_name : 'Lineaunope', count: 7 }, function(err, reply) {	
+ 		response.end(JSON.stringify(reply));
+	});	
+});
+
+route.get('/USGSted', function(request, response) {
+	response.writeHead(200, {"Content-Type": "application/json"});
+	twitter.get('statuses/user_timeline', { screen_name : 'USGSted', count: 7 }, function(err, reply) {	
+ 		response.end(JSON.stringify(reply));
+	});	
+});
+
+route.get('/USGSBigQuakes', function(request, response) {
+	response.writeHead(200, {"Content-Type": "application/json"});
+	twitter.get('statuses/user_timeline', { screen_name : 'USGSBigQuakes', count: 7 }, function(err, reply) {	
  		response.end(JSON.stringify(reply));
 	});	
 });
